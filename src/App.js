ebbdf2e48f135cbb03cc2499fc4e7088ff1600d2
Steve Renner <che1111@hotmail.com>
2020-10-16 13:11:52 -0700

    State updating

--- src/App.js
+++ src/App.js
@@ -31,21 +31,27 @@ const App = () => {
         - if the `id` of the post matches `postId`, return a new post object with the desired values (use the spread operator).
         - otherwise just return the post object unchanged.
      */
-    const plusOne = posts.map((post) => {
-      if (post.id === postId) {
-        post.likes +=1
-      }
-      return plusOne
-    })
-    setPosts(plusOne)
-  };
+    // const newPost = posts.map((post) => {
+    //   if (post.id === postId) {
+    //     post.likes +=1
+    //   }
+    //   return newPost
+    // })
+    // setPosts(newPost)
+    setPosts(posts.map(post => {
+      if (postId === post.id) {
+        return {...post, likes: post.likes + 1}
+      } 
+      return post;
+    }))
+  }
 
   return (
     <div className='App'>
       {/* Add SearchBar and Posts here to render them */}
       {/* Check the implementation of each component, to see what props they require, if any! */}
       <SearchBar />
-      <Posts posts={posts} likePost={likePost} />
+      <Posts likePost = {likePost} posts = {posts} />
     </div>
   );
 };
--- src/components/Posts/Post.js
+++ src/components/Posts/Post.js
@@ -5,32 +5,29 @@ import PostHeader from './PostHeader';
 
 const Post = (props) => {
   // 🔥 Make sure the parent of Post is passing the right props!
-  const { likes, setLikes } = useState(props.post.likes)
+  const { post, likePost } = props;
 
-  const moreLikes = () => {
-    setLikes((likes) => likes+1);
-  }
 
   return (
     <div className='post-border'>
       <PostHeader
-        username={props.post.username}
-        thumbnailUrl={props.post.thumbnailUrl}
+        username={post.username}
+        thumbnailUrl={post.thumbnailUrl}
       />
       <div className='post-image-wrapper'>
         <img
           alt='post thumbnail'
           className='post-image'
-          src={props.post.imageUrl}
+          src={post.imageUrl}
         />
       </div>
       {/* Is LikeSection getting all the props it needs to work correctly? */}
-      <LikeSection likes = {likes} moreLikes = {moreLikes} />
+      <LikeSection likePost={() => likePost(post.id)} numberOfLikes={post.likes} />
       {/* Comments also wants its props! */}
       <Comments 
-        postId={props.post.imageUrl}
-        comments={props.post.comments}
-        timeStamp = {props.post.timeStamp}
+        // postId={props.post.imageUrl}
+        comments={post.comments}
+        // timeStamp = {props.post.timeStamp}
       />
     </div>
   );
